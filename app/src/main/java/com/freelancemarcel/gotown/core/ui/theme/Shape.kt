7ee03c7359826bc9bbd265567c43847803870fcb
Mark Marcel <freelancemarcel18@gmail.com>
2022-09-20 18:13:15 +0000

    list_of_houses - added shape for ListItemHouse.kt

--- app/src/main/java/com/freelancemarcel/gotown/core/ui/theme/Shape.kt
+++ app/src/main/java/com/freelancemarcel/gotown/core/ui/theme/Shape.kt
@@ -2,10 +2,89 @@ package com.freelancemarcel.gotown.core.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+
+class HouseListItemShape(private val cornerRadius: Float) : Shape {
+
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        return Outline.Generic(
+            path = drawHouseListItemShapePath(size = size, cornerRadius = cornerRadius)
+        )
+    }
+}
 
 val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
 )
+
+private fun drawHouseListItemShapePath(size: Size, cornerRadius: Float): Path {
+    return Path().apply {
+        reset()
+        // Top left arc
+        arcTo(
+            rect = Rect(
+                left = -cornerRadius,
+                top = -cornerRadius,
+                right = cornerRadius,
+                bottom = cornerRadius
+            ),
+            startAngleDegrees = 90.0f,
+            sweepAngleDegrees = -90.0f,
+            forceMoveTo = false
+        )
+        lineTo(x = size.width - cornerRadius, y = 0f)
+        // Top right arc
+        arcTo(
+            rect = Rect(
+                left = size.width - cornerRadius,
+                top = -cornerRadius,
+                right = size.width + cornerRadius,
+                bottom = cornerRadius
+            ),
+            startAngleDegrees = 180.0f,
+            sweepAngleDegrees = -90.0f,
+            forceMoveTo = false
+        )
+        lineTo(x = size.width, y = size.height - cornerRadius)
+        // Bottom right arc
+        arcTo(
+            rect = Rect(
+                left = size.width - cornerRadius,
+                top = size.height - cornerRadius,
+                right = size.width + cornerRadius,
+                bottom = size.height + cornerRadius
+            ),
+            startAngleDegrees = 270.0f,
+            sweepAngleDegrees = -90.0f,
+            forceMoveTo = false
+        )
+        lineTo(x = cornerRadius, y = size.height)
+        // Bottom left arc
+        arcTo(
+            rect = Rect(
+                left = -cornerRadius,
+                top = size.height - cornerRadius,
+                right = cornerRadius,
+                bottom = size.height + cornerRadius
+            ),
+            startAngleDegrees = 0.0f,
+            sweepAngleDegrees = -90.0f,
+            forceMoveTo = false
+        )
+        lineTo(x = 0f, y = cornerRadius)
+        close()
+    }
+}
--- app/src/main/java/com/freelancemarcel/gotown/houses/models/ListItemHouse.kt
+++ app/src/main/java/com/freelancemarcel/gotown/houses/models/ListItemHouse.kt
@@ -7,16 +7,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import com.freelancemarcel.gotown.core.ui.theme.HouseListItemShape
 
 @Composable
 fun ListItemHouse(house: HouseListItem, onViewHouse: (String) -> Unit){
-    Card(Modifier.fillMaxWidth().clickable { onViewHouse(house.url) }) {
-        Column(Modifier.fillMaxWidth().padding(8.dp)) {
+    Card(Modifier.fillMaxWidth().clip(HouseListItemShape(cornerRadius = LocalDensity.current.run { 24.dp.toPx() })).clickable { onViewHouse(house.url) }) {
+        Column(Modifier.fillMaxWidth().padding(horizontal = 32.dp, vertical = 8.dp)) {
             Text(text = house.name,style = MaterialTheme.typography.h6.copy(fontFamily = FontFamily.Serif))
-            Spacer(Modifier.height(8.dp))
             Text(text = house.region, style = MaterialTheme.typography.caption.copy(color = Color.White.copy(alpha = 0.8f),fontFamily = FontFamily.Serif))
         }
     }
